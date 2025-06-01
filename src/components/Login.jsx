80cb9789b0c2913251bbacfcb205934ccff7a20c
Marion Saru <maghangasaru@gmail.com>
2025-06-01 19:18:17 +0300

    Updated Main branch

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -3,6 +3,7 @@ import { useNavigate } from 'react-router-dom'
 import { useAuth } from './AuthContext'
 import LoginForm from './LoginForm'
 import HomeNavbar from './HomeNavbar'
+import styles from '../assets/css/Login.module.css'
 
 const Login = () => {
   const navigate = useNavigate()
@@ -18,14 +19,21 @@ const Login = () => {
   }
 
   return (
-    <div className="auth-page">
+    <div>
+      <div className={styles["navbar-container"]}>
         <HomeNavbar />
-      <h1>Login</h1>
-      {error && <p className="error">{error}</p>}
-      <LoginForm onLogin={handleLogin} />
-      <p className="auth-switch">
-        Don't have an account? <a href="/signup">Sign up</a>
-      </p>
+      </div>
+
+      <div className={styles["auth-page"]}>
+        <div>
+          <h1>Login</h1>
+          {error && <p className={styles["error"]}>{error}</p>}
+          <LoginForm onLogin={handleLogin} />
+          <p className={styles["auth-switch"]}>
+            Don't have an account? <a href="/signup">Sign up</a>
+          </p>
+        </div>
+      </div>
     </div>
   )
 }
