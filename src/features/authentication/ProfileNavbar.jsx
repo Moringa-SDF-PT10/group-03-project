fdde5fca45a885253bce7fc27e02e5f6dd79ce77
Joseph Mwangi <josymuramu@gmail.com>
2025-06-01 14:05:45 +0300

    addedd a handleLogout function in ProfileNavbar component

--- src/features/authentication/ProfileNavbar.jsx
+++ src/features/authentication/ProfileNavbar.jsx
@@ -1,14 +1,21 @@
-import { Link } from "react-router-dom";
+import { Navigate, useNavigate } from "react-router-dom";
 import styles from "./ProfileNavbar.module.css";
+import { use } from "react";
 
 function ProfileNavbar() {
+    const navigate = useNavigate();
+
+    function handleLogout() {
+        localStorage.removeItem("token"); // clearing authentication tokens
+        navigate("/login"); // redirecting to login page
+    }
     return (
         <nav className={styles.navbar}>
             <div className={styles.logo}>
                 <h1>👤 My Profile</h1>
             </div>
-            <div className={styles["nav-links"]}>
-                <Link to="logout">Logout</Link>
+            <div>
+                <button onClick={handleLogout} className={styles.logoutBtn}>Logout</button>
             </div>
         </nav>
     );
